e44f76e98b8d7d46753da9fcc06792cd17ccf4ed
unknown <gy_ch@yahoo.com>
2023-05-17 07:12:09 +0800

    First Version Completed

--- ParkingApp/Program.cs
+++ ParkingApp/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+Console.Write("Please input total number of parking hours: ");
+String? input = Console.ReadLine();
+int result;
+
+if (string.IsNullOrEmpty(input))
+{
+    Console.WriteLine("Input cannot be empty");
+}
+else if (!Regex.IsMatch(input, @"^\d+$")) {
+    Console.WriteLine("Input must be a positive integer");
+}
+else {
+    int numberOfHours = int.Parse(input);
+
+    if (numberOfHours ==  0) {
+        result = 0;
+    }
+    else if (numberOfHours == 1) {
+        result = 2;
+    }
+    else if (numberOfHours == 2) {
+        result = 2 + 3;
+    }
+    else if (numberOfHours == 3) {
+        result = 2 + 3 * 2;
+    }
+    else {
+        result = 2 + 3 * 2 + 5 * (numberOfHours - 3);
+    }
+
+    Console.WriteLine($"Total parking fees: ${result}");
+}
