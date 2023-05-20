16d0237621338ae69cb840796495ed48c054d60a
unknown <gy_ch@yahoo.com>
2023-05-20 15:49:56 +0800

    Fifth Version Completed

--- ParkingApp/Program.cs
+++ ParkingApp/Program.cs
@@ -5,7 +5,7 @@ Console.Write("Please input start time: ");
 String? startTime = Console.ReadLine();
 Console.Write("Please input end time: ");
 String? endTime = Console.ReadLine();
-int result;
+decimal result;
 DateTime startTimeValue, endTimeValue;
 
 if (string.IsNullOrEmpty(startTime) || string.IsNullOrEmpty(endTime))
@@ -23,36 +23,41 @@ else {
     endTimeValue = DateTime.ParseExact(endTime, "dd/MM/yyyy hh:mm:ss tt", CultureInfo.InvariantCulture);
     int numberOfHours = (int)Math.Ceiling((endTimeValue - startTimeValue).TotalHours);
 
+    decimal firstHourRate = Math.Round(2 * 1.10M, 2);
+    decimal secondAndThirdHourRate = Math.Round(3 * 1.10M, 2);
+    decimal fourthAndAboveHourRate = Math.Round(5 * 1.10M, 2);
+    decimal dailyRate = Math.Round(70 * 1.10M, 2);
+
     if (numberOfHours ==  0) {
         result = 0;
     }
     else if (numberOfHours == 1) {
-        result = 2;
+        result = firstHourRate;
     }
     else if (numberOfHours == 2) {
-        result = 2 + 3;
+        result = firstHourRate + secondAndThirdHourRate;
     }
     else if (numberOfHours == 3) {
-        result = 2 + 3 * 2;
+        result = firstHourRate + secondAndThirdHourRate * 2;
     }
     else if (numberOfHours < 24) {
-        if (2 + 3 * 2 + 5 * (numberOfHours - 3) > 70) {
-            result = 70;
+        if (firstHourRate + secondAndThirdHourRate * 2 + fourthAndAboveHourRate * (numberOfHours - 3) > dailyRate) {
+            result = dailyRate;
         }
         else {
-            result = 2 + 3 * 2 + 5 * (numberOfHours - 3);
+            result = firstHourRate + secondAndThirdHourRate * 2 + fourthAndAboveHourRate * (numberOfHours - 3);
         }
     }
     else {
-        int remainingHoursFees;
-        if (numberOfHours % 24 * 5 > 70) {
-            remainingHoursFees = 70;
+        decimal remainingHoursFees;
+        if (numberOfHours % 24 * fourthAndAboveHourRate > dailyRate) {
+            remainingHoursFees = dailyRate;
         }
         else {
-            remainingHoursFees = numberOfHours % 24 * 5;
+            remainingHoursFees = numberOfHours % 24 * fourthAndAboveHourRate;
         }
-        result = (numberOfHours / 24) * 70 + remainingHoursFees;
+        result = (numberOfHours / 24) * dailyRate + remainingHoursFees;
     }
 
-    Console.WriteLine($"Total parking fees: ${result}");
+    Console.WriteLine($"Total parking fees: ${result.ToString("0.00")}");
 }
