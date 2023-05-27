91107543eaf51f25506512ce94396b080c795993
unknown <gy_ch@yahoo.com>
2023-05-27 19:00:26 +0800

    Add try catch in Program.cs

--- ParkingApp/Program.cs
+++ ParkingApp/Program.cs
@@ -6,5 +6,17 @@ Console.Write("Please input end time: ");
 String? endTime = Console.ReadLine();
 
 ParkingFeesCalculator parkingFeesCalculator = new ParkingFeesCalculator();
-decimal result = parkingFeesCalculator.CalculateParkingFees(startTime, endTime);
-Console.WriteLine($"Total parking fees: ${result.ToString("0.00")}");
+try
+{
+    decimal result = parkingFeesCalculator.CalculateParkingFees(startTime, endTime);
+    Console.WriteLine($"Total parking fees: ${result.ToString("0.00")}");
+}
+catch (Exception ex)
+{
+    if (ex is ArgumentException || ex is InvalidCastException || ex is InvalidDataException)
+    {
+        Console.WriteLine(ex.Message);
+        return;
+    }
+    throw;
+}
