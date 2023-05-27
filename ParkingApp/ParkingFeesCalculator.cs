7aa478a5add31afe2814127372c09268be2f6119
unknown <gy_ch@yahoo.com>
2023-05-27 17:27:41 +0800

    Refactoring: moving parking fees calculations into another method

--- ParkingApp/ParkingFeesCalculator.cs
+++ ParkingApp/ParkingFeesCalculator.cs
@@ -6,7 +6,6 @@ public class ParkingFeesCalculator
 {
     public decimal CalculateParkingFees(string? startTime, string? endTime)
     {
-        decimal result;
         DateTime startTimeValue, endTimeValue;
 
         if (string.IsNullOrEmpty(startTime) || string.IsNullOrEmpty(endTime))
@@ -32,56 +31,63 @@ public class ParkingFeesCalculator
             }
             else
             {
-                int numberOfHours = (int)Math.Ceiling((endTimeValue - startTimeValue).TotalHours);
-
-                decimal firstHourRate = Math.Round(2 * 1.10M, 2);
-                decimal secondAndThirdHourRate = Math.Round(3 * 1.10M, 2);
-                decimal fourthAndAboveHourRate = Math.Round(5 * 1.10M, 2);
-                decimal dailyRate = Math.Round(70 * 1.10M, 2);
-
-                if (numberOfHours == 0)
-                {
-                    result = 0;
-                }
-                else if (numberOfHours == 1)
-                {
-                    result = firstHourRate;
-                }
-                else if (numberOfHours == 2)
-                {
-                    result = firstHourRate + secondAndThirdHourRate;
-                }
-                else if (numberOfHours == 3)
-                {
-                    result = firstHourRate + secondAndThirdHourRate * 2;
-                }
-                else if (numberOfHours < 24)
-                {
-                    if (firstHourRate + secondAndThirdHourRate * 2 + fourthAndAboveHourRate * (numberOfHours - 3) > dailyRate)
-                    {
-                        result = dailyRate;
-                    }
-                    else
-                    {
-                        result = firstHourRate + secondAndThirdHourRate * 2 + fourthAndAboveHourRate * (numberOfHours - 3);
-                    }
-                }
-                else
-                {
-                    decimal remainingHoursFees;
-                    if (numberOfHours % 24 * fourthAndAboveHourRate > dailyRate)
-                    {
-                        remainingHoursFees = dailyRate;
-                    }
-                    else
-                    {
-                        remainingHoursFees = numberOfHours % 24 * fourthAndAboveHourRate;
-                    }
-                    result = (numberOfHours / 24) * dailyRate + remainingHoursFees;
-                }
-                
-                return result;
+                return Calculate(startTimeValue, endTimeValue);
             }
         }
+    }
+
+    public decimal Calculate(DateTime startTimeValue, DateTime endTimeValue)
+    {
+        int numberOfHours = (int)Math.Ceiling((endTimeValue - startTimeValue).TotalHours);
+
+        decimal firstHourRate = Math.Round(2 * 1.10M, 2);
+        decimal secondAndThirdHourRate = Math.Round(3 * 1.10M, 2);
+        decimal fourthAndAboveHourRate = Math.Round(5 * 1.10M, 2);
+        decimal dailyRate = Math.Round(70 * 1.10M, 2);
+
+        decimal result;
+
+        if (numberOfHours == 0)
+        {
+            result = 0;
+        }
+        else if (numberOfHours == 1)
+        {
+            result = firstHourRate;
+        }
+        else if (numberOfHours == 2)
+        {
+            result = firstHourRate + secondAndThirdHourRate;
+        }
+        else if (numberOfHours == 3)
+        {
+            result = firstHourRate + secondAndThirdHourRate * 2;
+        }
+        else if (numberOfHours < 24)
+        {
+            if (firstHourRate + secondAndThirdHourRate * 2 + fourthAndAboveHourRate * (numberOfHours - 3) > dailyRate)
+            {
+                result = dailyRate;
+            }
+            else
+            {
+                result = firstHourRate + secondAndThirdHourRate * 2 + fourthAndAboveHourRate * (numberOfHours - 3);
+            }
+        }
+        else
+        {
+            decimal remainingHoursFees;
+            if (numberOfHours % 24 * fourthAndAboveHourRate > dailyRate)
+            {
+                remainingHoursFees = dailyRate;
+            }
+            else
+            {
+                remainingHoursFees = numberOfHours % 24 * fourthAndAboveHourRate;
+            }
+            result = (numberOfHours / 24) * dailyRate + remainingHoursFees;
+        }
+
+        return result;
     }
 }
